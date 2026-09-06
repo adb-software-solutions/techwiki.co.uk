d621d4be4e1d895a882fba90d8794f5f58269e74
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-09-07 00:52:37 +0100

    refactor: Remove TechWiki category article ad placement.

--- website/src/app/(wiki)/[category]/[slug]/page.tsx
+++ website/src/app/(wiki)/[category]/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { FloatingEditButton } from "@/components/wiki/ArticleActions";
 import { ArticleActionsBar } from "@/components/wiki/ArticleActionsBar";
 import { MarkdownRenderer } from "@/components/wiki/MarkdownRenderer";
-import { ArticleAd } from "@/lib/analytics";
 import { getArticleByPath } from "@/lib/wiki/api";
 import {
     SiBluesky,
@@ -249,7 +248,6 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
             {/* Article content */}
             <div className="mb-12">
                 <MarkdownRenderer html={article.rendered_html} />
-                <ArticleAd />
             </div>
 
             {/* Article footer */}
